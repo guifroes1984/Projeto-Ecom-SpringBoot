2f0aa0f9885e9dcc9f78bf30db74369f773e0128
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-16 10:52:08 -0300

    Criando API de login e servico de detalhes do usuario.

---
+++ src/main/java/br/com/guifroes1984/ecom/services/jwt/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package br.com.guifroes1984.ecom.services.jwt;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.guifroes1984.ecom.entity.Usuario;
+import br.com.guifroes1984.ecom.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuario> optionalUsuario = userRepository.findFirstByEmail(username);
+		if (optionalUsuario.isEmpty()) throw new UsernameNotFoundException("Nome de usuário não encontrado", null);
+		return new org.springframework.security.core.userdetails.User(optionalUsuario.get().getEmail(), optionalUsuario.get().getPassword()
+		, new ArrayList<>());
+	}
+
+}
